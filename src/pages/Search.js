a9c618de5a25bd6cbeb7c70360165670ec960a95
coryschadt <coryschadt@Corys-MacBook-Pro-2.local>
2020-10-12 19:25:14 -0700

    First commit

---
+++ src/pages/Search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import { IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react'
+
+const Search = () =>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>
+          Search
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  </IonPage>
+
+export default Search
---
+++ src/pages/Trending.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import { IonPage, IonHeader, IonToolbar, IonTitle } from '@ionic/react'
+
+const Trending = () =>
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>
+          Trending
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+  </IonPage>
+
+export default Trending
+
